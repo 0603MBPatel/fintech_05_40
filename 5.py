2c09510b9f43e86c4c26cf8207aa5c1fb12ab993
mayur <shreeji.5291@gmail.com>
2023-06-27 08:15:19 +0530

    5

---
+++ 5.py
@@ -0,0 +1,18 @@
+# list_manipulation.py
+fruits = ["apple", "banana", "cherry"]
+
+# Accessing elements
+print("First fruit:", fruits[0])
+print("Second fruit:", fruits[1])
+
+# Modifying elements
+fruits[1] = "orange"
+
+# Adding elements
+fruits.append("mango")
+
+# Removing elements
+fruits.remove("cherry")
+
+# Printing the modified list
+print("Updated fruits:", fruits)
